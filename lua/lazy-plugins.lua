7c84e9fc7e6def719b77cd2fb174c28954bb129b
Tomas Oh <tomasoh@csu.fullerton.edu>
2024-05-22 14:15:44 -0700

    Modularize nvim config

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,63 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- NOTE: Plugins can also be added by using a table,
+  -- with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  --
+  -- Use `opts = {}` to force a plugin to be loaded.
+  --
+  --  This is equivalent to:
+  --    require('Comment').setup({})
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  require "custom/plugins/cmp",
+  require "custom/plugins/conform",
+  require "custom/plugins/debug",
+  require "custom/plugins/gitsigns",
+  require "custom/plugins/indent_line",
+  require "custom/plugins/lint",
+  require "custom/plugins/lspconfig",
+  require "custom/plugins/mini",
+  require "custom/plugins/onedark",
+  require "custom/plugins/telescope",
+  require "custom/plugins/todo-comments",
+  require "custom/plugins/tresitter",
+  require "custom/plugins/which-key",
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
