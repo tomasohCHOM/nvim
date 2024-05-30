2dbdb3542cddae64d52b1c970c752cc587dc8da1
Tomas Oh <tomasoh@csu.fullerton.edu>
2024-05-30 12:05:48 -0700

    Add keymap to remove ^M

--- init.lua
+++ init.lua
@@ -80,6 +80,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Delete the ^M that appears when pasting from an external source (Windows moment)
+vim.keymap.set('n', '<leader>dn', [[<cmd>%s/\r//g<CR>]])
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -117,7 +120,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-N>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-N>', { noremap = true, silent = true })
 
 require 'lazy-bootstrap'
 require 'lazy-plugins'
