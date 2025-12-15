af6a3dbf946254b72a36949ccc5eb87322165a4a
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-12-15 13:06:23 -0800

    start config clean up

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
--- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -11,11 +10,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Require all modules inside lua/
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
 
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
