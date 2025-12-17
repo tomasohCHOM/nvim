bc4d4e51b3b93e5e8e55066e7d5e26f10d3700c8
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-12-17 10:06:58 -0800

    add prettierd default formatter

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -2,23 +2,25 @@ return { -- Autoformat
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters = {
+      black = {
+        prepend_args = { '--fast' },
+      },
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'black' },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      javascript = { 'prettierd' },
+      typescript = { 'prettierd' },
+      css = { 'prettierd' },
+      html = { 'prettierd' },
+      json = { 'prettierd' },
+      yaml = { 'prettierd' },
+      markdown = { 'prettierd' },
     },
   },
 }
